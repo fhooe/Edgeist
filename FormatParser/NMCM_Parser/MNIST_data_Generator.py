632d63911ad4243ebb33bb22b2d88127f7683e8c
Tobias Egger <t.egger@danube-dynamics.at>
2025-12-16 12:09:02 +0100

    chore: add MNIST_data_Generator.py

---
+++ FormatParser/NMCM_Parser/MNIST_data_Generator.py
@@ -0,0 +1,33 @@
+import struct
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+
+# generates train and test data (the mnist_samples ar in a random order) to use with the Desktop_Application
+
+# output files
+output_file_train = "mnist_train_all_random.bin"
+output_file_test = "mnist_test_all_random.bin"
+
+# MNIST dataset with ToTensor ([0.0, 1.0])
+transform = transforms.ToTensor()
+mnist_train = datasets.MNIST(root=".", train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(root=".", train=False, download=True, transform=transform)
+
+# Random permutation of training data
+indices_train = torch.randperm(len(mnist_train))
+with open(output_file_train, "wb") as f:
+    for idx in indices_train:
+        img_tensor, label = mnist_train[idx]
+        flat = img_tensor.squeeze().flatten().numpy().astype(np.float32)
+        f.write(struct.pack("<784f", *flat))
+        f.write(struct.pack("B", label))
+
+# Random permutation of test data
+indices_test = torch.randperm(len(mnist_test))
+with open(output_file_test, "wb") as f:
+    for idx in indices_test:
+        img_tensor, label = mnist_test[idx]
+        flat = img_tensor.squeeze().flatten().numpy().astype(np.float32)
+        f.write(struct.pack("<784f", *flat))
+        f.write(struct.pack("B", label))
